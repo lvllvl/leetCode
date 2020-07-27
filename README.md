2dcb9bfe947700101686d6b531bc86b5bfdb2abf
lvllvl <24905907+lvllvl@users.noreply.github.com>
2020-07-27 13:46:23 -0400

    add new files

---
+++ README.md
@@ -0,0 +1,1 @@
+if I work on a leetcode problem, I'm going to try and post here
---
+++ shuffleString.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def __init__( self ):
+        self.self = self
+
+    
+    def restoreString( self, s, indices ):
+        res = ''
+
+        for i in range( len( indices ) ):
+
+            res += s[ indices.index( i ) ]
+        
+        return res 
+
+s = 'codeleet'
+indices = [ 4, 5, 6, 7, 0, 2, 1, 3 ]
+
+sol = Solution()
+print( sol.restoreString( s, indices ))
+
