872718995c46dba00aa61c3a7f3a0878e09c4aa9
lvllvl <24905907+lvllvl@users.noreply.github.com>
2020-07-27 15:59:40 -0400

    new lC problem

---
+++ 1431Candies.py
@@ -0,0 +1,19 @@
+def extraCandies( candies, extraCandies ):
+    output = [] 
+
+    for i in range( len( candies ) ): 
+        maxNum = candies[ i ] + extraCandies
+
+        output.append( ( maxNum >= max( candies ) ) )
+
+        maxNum = 0
+
+    return output 
+  
+
+candies = [ 4, 2, 1, 1, 2 ]
+extraCandies = 1
+
+print( extraCandies  )
+extraCandies( candies, extraCandies )
+
